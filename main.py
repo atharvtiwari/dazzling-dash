a5818786bd453140d93078f5a1fa462902a11758
atharvtiwari <atharv.tiwari@outlook.com>
2021-02-27 13:17:46 +0530

    Cleaned up unnecessary lines of code and removed dynamic resizing

--- main.py
+++ main.py
@@ -9,8 +9,7 @@ clock = pygame.time.Clock()
 
 WIDTH = 1280
 HEIGHT = 720
-screen = pygame.display.set_mode((WIDTH, HEIGHT), HWSURFACE|DOUBLEBUF|RESIZABLE)
-fake_screen = screen.copy()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
 filepath = os.getcwd()
 track_image = pygame.image.load(os.path.join(filepath+"/images", "track.png")).convert_alpha()
@@ -25,15 +24,17 @@ redcar = REDCAR_ORIGINAL
 mask_red = pygame.mask.from_surface(redcar)
 off_mask = pygame.mask.from_surface(track_image)
 off_mask.invert()
-s = False
 run = True
 
 while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-        elif event.type == VIDEORESIZE:
-            screen = pygame.display.set_mode(event.size, HWSURFACE|DOUBLEBUF|RESIZABLE)
+    
+    vel_red = Vector2(redspeed, 0)
+    vel_red.rotate_ip(-redangle)
+    redcar = pygame.transform.rotate(REDCAR_ORIGINAL, redangle)
+    mask_red = pygame.mask.from_surface(redcar)
 
     if redspeed >= 0.02:
         redspeed -= 0.02
@@ -41,61 +42,47 @@ while run:
         redspeed += 0.01
     elif redspeed < 0.02 or redspeed >=-0.01:
         redspeed = 0
-    vel_red = Vector2(redspeed, 0)
-    vel_red.rotate_ip(-redangle)
-    redcar = pygame.transform.rotate(REDCAR_ORIGINAL, redangle)
-    mask_red = pygame.mask.from_surface(redcar)
 
     keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_ESCAPE]:
+    if keys[pygame.K_q]:
         break
 
-    if keys[pygame.K_UP]:
-        if redspeed < 7:
+    if keys[pygame.K_UP] or keys[pygame.K_w]:
+        if redspeed <= 9.95:
             redspeed += 0.05
-        vel_red = Vector2(redspeed, 0)
-        vel_red.rotate_ip(-redangle)
-        redcar = pygame.transform.rotate(REDCAR_ORIGINAL, redangle)
-        mask_red = pygame.mask.from_surface(redcar)
     
-    if keys[pygame.K_DOWN]:
-        if redspeed > 0:
-            redspeed -= 0.10
-        elif redspeed > -2:
+    if keys[pygame.K_DOWN] or keys[pygame.K_s]:
+        if redspeed >= 0.1:
+            redspeed -= 0.1
+        elif redspeed >= -2.975:
             redspeed -= 0.025
-        vel_red = Vector2(redspeed, 0)
-        vel_red.rotate_ip(-redangle)
-        redcar = pygame.transform.rotate(REDCAR_ORIGINAL, redangle)
-        mask_red = pygame.mask.from_surface(redcar)
 
-    if keys[pygame.K_LEFT]:
+    if keys[pygame.K_LEFT] or keys[pygame.K_a]:
         if vel_red.magnitude_squared() > 0.004:
             redangle += 2.5
             vel_red.rotate_ip(-2.5)
-        redcar = pygame.transform.rotate(REDCAR_ORIGINAL, redangle)
-        mask_red = pygame.mask.from_surface(redcar)
 
-    if keys[pygame.K_RIGHT]:
+    if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
         if vel_red.magnitude_squared() > 0.004:
             redangle -= 2.5
             vel_red.rotate_ip(2.5)
-        redcar = pygame.transform.rotate(REDCAR_ORIGINAL, redangle)
-        mask_red = pygame.mask.from_surface(redcar)
+    
+    if keys[pygame.K_SPACE]:
+        redspeed = 0
 
     if (pos_red.x <= 0) or (pos_red.x >= WIDTH) or (pos_red.y <= 0) or (pos_red.y >= HEIGHT) or keys[pygame.K_e]:
         redangle = 0
-        redspeed = 0.0
+        redspeed = 0
         pos_red = Vector2(277,87.5)
-        vel_red = Vector2(redspeed, 0)
     
     pos_red += vel_red
     redcar_pos = list(int(v) for v in pos_red)
 
     offtrack = off_mask.overlap(mask_red, redcar_pos)
 
-    fake_screen.fill(pygame.Color('darkgreen'))
-    fake_screen.blit(track_image, (0, 0))
+    screen.fill(pygame.Color('darkgreen'))
+    screen.blit(track_image, (0, 0))
 
     if offtrack:
         if vel_red.magnitude_squared() > 0.25:
@@ -103,15 +90,11 @@ while run:
                 redspeed -= 0.25
             else:
                 redspeed += 0.25
-        vel_red = Vector2(redspeed, 0)
-        vel_red.rotate_ip(-redangle)
-        redcar = pygame.transform.rotate(REDCAR_ORIGINAL, redangle)
-        mask_red = pygame.mask.from_surface(redcar)
 
-    print(pos_red, vel_red)
+    if redspeed != 0:
+        print(pos_red, vel_red)
 
-    fake_screen.blit(redcar, redcar_pos)
-    screen.blit(pygame.transform.scale(fake_screen, screen.get_rect().size), (0, 0))
+    screen.blit(redcar, redcar_pos)
     pygame.display.flip()
     clock.tick(60)
 
